162b3fc3cac59b972d9e697b366fb69878c7dc26
Mona Fakhri <96212168+fakhrisalem@users.noreply.github.com>
2025-09-13 16:17:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var currentImage=0;
 var tiomeid;
 //array name for images 
 var imagearray=[];
-imagearray[0]="./photo/12.jpg";
-imagearray[1]="./photo/11.png";
-imagearray[2]="./photo/1.png";
-imagearray[3]="./photo/2.png";
-imagearray[4]="./photo/3.png";
-imagearray[5]="./photo/4.jpg";
+imagearray[0]="12.jpg";
+imagearray[1]="11.png";
+imagearray[2]="1.png";
+imagearray[3]="2.png";
+imagearray[4]="3.png";
+imagearray[5]="4.jpg";
 function nextimage(){//function to change image 
    currentImage++;
    if(currentImage>5) 
